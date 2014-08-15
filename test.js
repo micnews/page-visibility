f60042ee58e3c901f30d3ed450ace9dab059de13
David Björklund <david.bjorklund@gmail.com>
2014-08-15 16:03:35 -0400

    rewrite test to work in IE8

--- test.js
+++ test.js
@@ -1,20 +1,6 @@
 var test = require('tape')
   , getVisibility = require('./page-visibility')
 
-test('when window is visible', function (t) {
-  getVisibility(function (visible) {
-    t.equal(visible, true, 'visible should be true')
-    t.end()
-  })
-
-  // manually dispatch a mousemove event to support older browsers
-  // can not use new MouseEvent cause phantomjs don't support it
-  var event = document.createEvent('MouseEvent')
-  event.initMouseEvent('mousemove', false, false, null)
-
-  document.dispatchEvent(event)
-})
-
 // only test this when we're running an older browser that don't support the HTML5 api
 if (document.webkitHidden === undefined && document.mozHidden === undefined && document.webkitHidden === undefined)
   test('when window is not visible', function (t) {
@@ -23,3 +9,28 @@ if (document.webkitHidden === undefined && document.mozHidden === undefined && d
       t.end()
     })
   })
+
+test('when window is visible', function (t) {
+  getVisibility(function (visible) {
+    t.equal(visible, true, 'visible should be true')
+    t.end()
+  })
+
+  // manually dispatch a mousemove event to support older browsers
+  // can not use new MouseEvent cause phantomjs don't support it
+  var event
+
+  if (document.createEvent) {
+    event = document.createEvent
+    event.initMouseEvent('mousemove', false, false, null)
+    document.dispatchEvent(event)
+  }
+  else {
+    // MSIE
+    event = document.createEventObject('MouseEvent')
+    console.log('2')
+    document.fireEvent('onmousemove', event)
+    console.log('3')
+  }
+
+})
